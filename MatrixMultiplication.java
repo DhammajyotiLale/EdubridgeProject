1ee09d4511e37d81e281a1284189d7409107bd86
DhammajyotiLale <101916748+DhammajyotiLale@users.noreply.github.com>
2022-03-22 22:39:51 +0530

    Add files via upload
    file added

---
+++ MatrixMultiplication.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class MatrixMultiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//creating two matrix
+		int arr1[][]= {{1,3,6},{3,8,9},{4,8,6}};//creating two matrix
+		int arr2[][]= {{4,9,5},{4,2,7},{1,9,5}};
+
+
+		int arr3[][]=new int[3][3];
+
+
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<3;j++) {
+				arr3[i][j]=arr1[i][j]*arr2[i][j];
+				System.out.println(arr3[i][j]+" ");
+			}
+			System.out.println();
+			
+		}
+
+			}
+
+		}
+
+
+
+
